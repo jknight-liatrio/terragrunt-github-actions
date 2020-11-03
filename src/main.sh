929c37aadf84c5dc0febc9cb7930e3c5bc1cebf9
Joe Knight <joek@liatrio.com>
2020-11-03 16:02:17 -0600

    Adding debugging

--- src/main.sh
+++ src/main.sh
@@ -139,7 +139,7 @@ function installTerragrunt {
 
   echo "Moving Terragrunt ${tgVersion} to PATH"
   chmod +x /tmp/terragrunt
-  mv /tmp/terragrunt /usr/local/bin/terragrunt 
+  mv /tmp/terragrunt /usr/local/bin/terragrunt
   if [ "${?}" -ne 0 ]; then
     echo "Failed to move Terragrunt ${tgVersion}"
     exit 1
@@ -164,6 +164,7 @@ function main {
   configureCLICredentials
   installTerraform
   cd ${GITHUB_WORKSPACE}/${tfWorkingDir}
+  ls -al
 
   case "${tfSubcommand}" in
     fmt)
